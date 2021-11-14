727a251c51a21c892ad7c75533045b0d002fb5ce
mader.laszlo@stud.uni-obuda.hu <mader.laszlo@stud.uni-obuda.hu>
2021-11-14 17:39:43 +0100

    Added IPurchaseLogic.cs and PurchaseLogic.cs, added CRUD to interface

---
+++ EY4J5D_HFT_2021221.Logic/IPurchaseLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EY4J5D_HFT_2021221.Logic
+{
+    interface IPurchaseLogic
+    {
+        public void Create(Purchase newModel);
+        public Purchase Read(int id);
+        public IEnumerable<Purchase> ReadAll();
+        public void Update(Purchase updated);
+        public void Delete(int id);
+    }
+}
